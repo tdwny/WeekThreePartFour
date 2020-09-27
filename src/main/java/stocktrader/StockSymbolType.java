621278a10e33bee06c08c446de365f6a58a2c8d9
Timothy Downey <timothy.downey@c02cg3z3md6p.home>
2020-09-27 19:21:59 -0400

    First

---
+++ src/main/java/stocktrader/StockSymbolType.java
@@ -0,0 +1,11 @@
+package stocktrader;
+
+/**
+ * A typesafe list of stock symbols
+ * (Feel free to add your favorites)
+ */
+public enum StockSymbolType {
+
+    APPL, IBM, GOOG, AMZ
+
+}
---
+++ src/main/java/stocktrader/StockTraderApp.java
@@ -0,0 +1,11 @@
+package stocktrader;
+
+/**
+ * Client code for the StockService application.
+ */
+public class StockTraderApp {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
